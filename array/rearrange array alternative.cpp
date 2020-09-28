61b4db5bf610e9a16cb2e62a93ead61f8a8b3a11
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-09-28 22:43:30 +0530

    Create rearrange array alternative.cpp

---
+++ array/rearrange array alternative.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+ {
+     int t;
+     cin>>t;
+     while(t--)
+     {
+         int n;
+         cin>>n;
+         int* arr = new int[n];
+         for(int i=0;i<n;i++){
+             cin>>arr[i];
+         }
+         int i=0;
+         int j=n-1;
+         while(i<=j){
+             if(i==j){
+                 cout<<arr[i];
+                 break;
+             }
+             cout<<arr[j]<<" "<<arr[i]<<" ";
+             i++;
+             j--;
+         }
+         cout<<endl;
+     }
+	//code
+	return 0;
+}
