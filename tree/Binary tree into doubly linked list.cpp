af94473fa6cb9b87c0a78d6595c402b4f6205f2f
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-10-07 22:31:25 +0530

    Create Binary tree into doubly linked list.cpp

---
+++ tree/Binary tree into doubly linked list.cpp
@@ -0,0 +1,39 @@
+static queue <Node *> q;
+void Inorder(struct Node *p)
+    {
+        if(p)
+        {
+            Inorder(p->left);
+            q.push(p);
+            Inorder(p->right);
+        }
+    }
+
+
+Node * bToDLL(Node *root)
+{
+    if(root == NULL || (root->right==NULL && root->left==NULL))
+    {
+        return root;
+    }
+    Inorder(root);
+    struct Node * temp;
+    struct Node * head=q.front();
+        temp=q.front();
+        q.pop();
+        temp->left=NULL;
+        temp->right =q.front();
+        q.front()->left =temp;
+    while(q.size()!=1)
+    {
+        temp=q.front();
+        q.pop();
+        temp->right=q.front();
+        q.front()->left=temp;
+    }
+    q.front()->left = temp;
+    q.front()->right =NULL;
+    q.pop();
+    return head;
+    // your code here
+}
