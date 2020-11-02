62853e33bea64ff22eab320413edad216101be04
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-11-02 22:02:47 +0530

    Create check BST.cpp

---
+++ tree/check BST.cpp
@@ -0,0 +1,174 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+
+bool isBST(struct Node* node);
+int isBSTUtil(struct Node* node, int min, int max);
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+   // Corner Case
+   if(str.length() == 0 || str[0] == 'N')
+       return NULL;
+
+   // Creating vector of strings from input
+   // string after spliting by space
+   vector<string> ip;
+
+   istringstream iss(str);
+   for(string str; iss >> str; )
+       ip.push_back(str);
+
+   // Create the root of the tree
+   Node* root = new Node(stoi(ip[0]));
+
+   // Push the root to the queue
+   queue<Node*> queue;
+   queue.push(root);
+
+   // Starting from the second element
+   int i = 1;
+   while(!queue.empty() && i < ip.size()) {
+
+       // Get and remove the front of the queue
+       Node* currNode = queue.front();
+       queue.pop();
+
+       // Get the current node's value from the string
+       string currVal = ip[i];
+
+       // If the left child is not null
+       if(currVal != "N") {
+
+           // Create the left child for the current node
+           currNode->left = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->left);
+       }
+
+       // For the right child
+       i++;
+       if(i >= ip.size())
+           break;
+       currVal = ip[i];
+
+       // If the right child is not null
+       if(currVal != "N") {
+
+           // Create the right child for the current node
+           currNode->right = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->right);
+       }
+       i++;
+   }
+
+   return root;
+}
+
+void inorder(Node *root, vector<int> &v)
+{
+    if(root==NULL)
+        return;
+
+    inorder(root->left, v);
+    v.push_back(root->data);
+    inorder(root->right, v);
+}
+
+int main() {
+ 
+   int t;
+   string tc;
+   getline(cin, tc);
+   t=stoi(tc);
+   while(t--)
+   {
+    string s; 
+    getline(cin, s);
+    Node* root = buildTree(s);
+    cout << isBST(root) << endl;
+   }
+   return 0;
+}
+
+
+
+ // } Driver Code Ends
+
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child  
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+// return true if the given tree is a BST, else return false
+bool isBST(Node* root) 
+{
+    if(root==NULL)
+    {
+        return true;
+    }
+    struct Node * temp;
+    queue <Node *> ql,qr;
+    if(root->left!=NULL)
+    ql.push(root->left);
+    if(root->right!=NULL)
+    qr.push(root->right);
+    while(!ql.empty())
+    {
+        temp=ql.front();
+        ql.pop();
+        if(( temp->left== NULL || (temp->left->data<temp->data && temp->left->data<root->data)) && ( temp->right== NULL || (temp->right->data>temp->data && temp->right->data<root->data))){}
+        else return false;
+        if(temp->left!=NULL)
+        ql.push(temp->left);
+        if(temp->right!=NULL)
+        ql.push(temp->right);
+    }
+    while(!qr.empty())
+    {
+        temp=qr.front();
+        qr.pop();
+        if(( temp->left== NULL || (temp->left->data<temp->data && temp->left->data>root->data)) && ( temp->right== NULL || (temp->right->data>temp->data && temp->right->data>root->data))){}
+        else return false;
+        if(temp->left!=NULL)
+        qr.push(temp->left);
+        if(temp->right!=NULL)
+        qr.push(temp->right);
+    }
+    return true;
+    // Your code here
+}
+
+
+
+
+// { Driver Code Starts
+  // } Driver Code Ends
