598148b9cce9d0dcfe6e8996c434153537ac9f50
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-11-23 22:53:16 +0530

    Create Last index of one .cpp

---
+++ array/Last index of one .cpp
@@ -0,0 +1,24 @@
+using namespace std;
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        int ans=-1;
+        for(int i=(s.length()-1);i>=0;i--)
+        {
+            if(s[i]=='1')
+            {
+                ans=i;
+                break;
+            }
+        }
+        cout<<ans<<endl;
+    }
+     
+	//code
+	return 0;
+}
