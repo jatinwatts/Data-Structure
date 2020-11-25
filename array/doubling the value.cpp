ff9919a6b7bfb39738606b0e7ef92463aed356c2
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-11-25 22:29:23 +0530

    Create doubling the value.cpp

---
+++ array/doubling the value.cpp
@@ -0,0 +1,21 @@
+ {
+     long long i,j,k,n,t;
+     cin>>t;
+     while(t--)
+     {
+         cin>>n>>k;
+         long long arr[n];
+         for(i=0;i<n;i++)
+         {
+             cin>>arr[i];
+            if(arr[i]==k)
+             {
+                 k=k*2;
+             } 
+         }
+
+         cout<<k<<endl;
+     }
+	//code
+	return 0;
+}
