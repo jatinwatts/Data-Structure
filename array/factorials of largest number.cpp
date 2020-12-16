1ef4ee8dcafc440a2ac5e5b60f31b2ed30961e9c
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-12-16 23:40:38 +0530

    Create factorials of largest number.cpp

---
+++ array/factorials of largest number.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+ {
+     int t;
+     cin>>t;
+     while(t--)
+     {
+         int n;
+         cin>>n;
+         vector<int> v;
+         v.push_back(1);
+         for(int x=2;x<=n;x++)
+         {
+             int carry=0;
+             for(int i=0;i<v.size();i++)
+             {
+                 int mul = v[i]*x+carry;
+                 v[i]=mul%10;
+                 carry=mul/10;
+             }
+             while(carry)
+             {
+                 v.push_back(carry%10);
+                 carry=carry/10;
+             }
+         }
+         
+         for(int i= v.size()-1;i>=0;i--)
+         {
+             cout<<v[i];
+         }
+         cout<<endl;
+         
+     }
+	//code
+	return 0;
+}
